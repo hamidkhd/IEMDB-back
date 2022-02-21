df877c12ccc19699b1389070d4ce70aa2dcba447
nazanin_ys <nazaninyousefian@yahoo.com>
2022-02-21 12:43:00 +0330

    invalidRateScore added

--- CA1/src/main/java/ir/ac/ut/ie/Vote.java
+++ CA1/src/main/java/ir/ac/ut/ie/Vote.java
@@ -1,13 +1,16 @@
 package ir.ac.ut.ie;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class Vote {
     private String userEmail;
     private Integer commentId;
     private int vote;
 
-    public boolean hasError() {
+    public boolean hasError() throws JsonProcessingException {
         if (vote == 1 || vote == -1 || vote == 0)
             return false;
+        CommandHandler.printOutput(new Output(false, "InvalidVoteValue"));
         return true;
     }
 
